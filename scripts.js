49042ed7fbfea2f93b374864097ed68991bf1d37
zakiabashir <nshafeys0@gmail.com>
2024-08-08 03:32:09 +0500

    push my code

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
